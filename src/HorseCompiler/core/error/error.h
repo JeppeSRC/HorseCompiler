99a4a0c98f47d826ec5e740fd94f2b59b764c129
JeppeSRC <theholyhorse.dev@gmail.com>
2020-07-23 18:39:59 +0200

    SImple assert

---
+++ src/HorseCompiler/core/error/error.h
@@ -0,0 +1,36 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jesper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+*/
+
+#pragma once
+
+#include <core/def.h>
+#include <core/log/log.h>
+
+#define HC_DEBUG_BREAK __debugbreak
+
+#ifdef HC_DEBUG
+#define HC_ASSERT(x, message) if (!(x)) { Log::Error("Assertion Failed: %s", message); HC_DEBUG_BREAK; }
+#else
+#define HC_ASSERT(x, message)
+#endif
