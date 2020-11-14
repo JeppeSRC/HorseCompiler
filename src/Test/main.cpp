23d88caf814c1c2a01aa562cf70f5fc6a9ecef7c
JeppeSRC <theholyhorse.dev@gmail.com>
2020-11-14 10:59:42 +0100

    Changed test

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -4,27 +4,29 @@
 #include <util/file.h>
 #include <core/error/error.h>
 #include <core/preprocessor/preprocessor.h>
+#include <core/compiler/compiler.h>
 
 #include <chrono>
+#include <Windows.h>
 
 int main(int argc, char** argv) {
-	Syntax syntax;
+	Syntax* syntax = Compiler::GetSyntax();
 
-	syntax.delimiters = " #=+-*/<>.,^&|(){}[]%\"'!?:;";
-	syntax.stringStart = '"';
-	syntax.stringEnd = '"';
-	syntax.numSequences = 2;
-	syntax.sequence = new EscapeSequence[3];
-	syntax.sequence[0].signature = 'n';
-	syntax.sequence[0].value = '\n';
-	syntax.sequence[1].signature = 'x';
-	syntax.sequence[1].base = 16;
-	
-	auto res = Lexer::Analyze("test.c", syntax);
+	syntax->delimiters = " #=+-*/<>.,^&|(){}[]%\"'!?:;";
+	syntax->stringStart = '"';
+	syntax->stringEnd = '"';
+	syntax->numSequences = 2;
+	syntax->sequence = new EscapeSequence[3];
+	syntax->sequence[0].signature = 'n';
+	syntax->sequence[0].value = '\n';
+	syntax->sequence[1].signature = 'x';
+	syntax->sequence[1].base = 16;
+
+	auto res = Lexer::Analyze("test.c");
 
 	List<String> includes;
 	
-	printf("%s\n", PreProcessor::Run(res, includes, syntax).str);
+	printf("%s\n", PreProcessor::Run(res, includes).str);
 
 	//Syntax::Analyze(syntax, res);
 
