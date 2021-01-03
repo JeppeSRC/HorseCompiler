985e2d7e81a36cb4c8bdd22922c8ee7eb99ddfa4
JeppeSRC <theholyhorse.dev@gmail.com>
2021-01-03 21:53:19 +0100

    Some kind of thing that's probablly gonna endup sucking the big D

---
+++ src/HorseCompiler/core/compiler/pattern.cpp
@@ -0,0 +1,89 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jesper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+*/
+
+#include "pattern.h"
+
+
+Pattern::VariablePattern::VariablePattern() {
+	PatternItem end({ TokenType::OpAssign, TokenType::Semicolon });
+	PatternItem name(TokenType::Identifier, end);
+
+	List<KeywordType> types = { KeywordType::Char, KeywordType::Short, KeywordType::Int,
+								KeywordType::Long, KeywordType::Float, KeywordType::Double,
+								KeywordType::Vec2, KeywordType::Vec3, KeywordType::Vec4,
+								KeywordType::Mat4 };
+
+	PatternItem cnst(TokenType::Keyword, KeywordType::Const);
+	PatternItem type({ TokenType::Keyword, TokenType::Identifier }, types);
+	PatternItem sign(TokenType::Keyword, { KeywordType::Signed, KeywordType::Unsigned }, type);
+
+	//<type> <name>
+	//<type> <const> <name>
+	{
+		auto t = type;
+		auto c = cnst;
+
+		c.next.PushBack(name);
+
+		t.next = { name, c };
+
+		first.PushBack(t);
+	}
+
+	//<const> <type> <name>
+	//<const> <sign> <type> <name>
+	{
+		auto t = type;
+		auto c = cnst;
+		auto s = sign;
+
+		t.next.PushBack(name);
+		s.next.PushBack(t);
+
+		c.next = { s, t };
+		first.PushBack(c);
+	}
+
+	//<sign> <type> <name>
+	//<sign> <type> <const> <name>
+	{
+		auto t = type;
+		auto c = cnst;
+		auto s = sign;
+
+		c.next.PushBack(name);
+		t.next = { name, c };
+		s.next.PushBack(t);
+
+		first.PushBack(s);
+	}
+
+
+}
+
+Pattern::VariablePattern::~VariablePattern() {
+	/*for (uint64 i = 0; i < first.GetSize(); i++) {
+		delete first[i];
+	}*/
+}
---
+++ src/HorseCompiler/core/compiler/pattern_patternitem.cpp
@@ -0,0 +1,87 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jesper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+*/
+
+#include "pattern.h"
+
+Pattern::PatternItem::PatternItem(TokenType token, const List<PatternItem>& next) : next(next) {
+	tokens.PushBack(token);
+}
+
+Pattern::PatternItem::PatternItem(TokenType token, const PatternItem& next) {
+	tokens.PushBack(token);
+	if (next.next.GetSize()) this->next.PushBack(next);
+}
+
+Pattern::PatternItem::PatternItem(const List<TokenType>& tokens, const List<PatternItem>& next) : tokens(tokens), next(next) {}
+
+Pattern::PatternItem::PatternItem(const List<TokenType>& tokens, const PatternItem& next) : tokens(tokens) {
+	if (next.next.GetSize()) this->next.PushBack(next);
+}
+
+
+
+Pattern::PatternItem::PatternItem(TokenType token, KeywordType keyword, const List<PatternItem>& next) : next(next) {
+	tokens.PushBack(token);
+	keywords.PushBack(keyword);
+}
+
+Pattern::PatternItem::PatternItem(TokenType token, KeywordType keyword, const PatternItem& next) {
+	tokens.PushBack(token);
+	if (next.next.GetSize()) this->next.PushBack(next);
+	keywords.PushBack(keyword);
+}
+
+Pattern::PatternItem::PatternItem(const List<TokenType>& tokens, KeywordType keyword, const List<PatternItem>& next) : tokens(tokens), next(next) {
+	keywords.PushBack(keyword);
+}
+
+Pattern::PatternItem::PatternItem(const List<TokenType>& tokens, KeywordType keyword, const PatternItem& next) : tokens(tokens) {
+	if (next.next.GetSize()) this->next.PushBack(next);
+	keywords.PushBack(keyword);
+}
+
+
+
+Pattern::PatternItem::PatternItem(TokenType token, const List<KeywordType>& keyword, const List<PatternItem>& next) : keywords(keyword), next(next) {
+	tokens.PushBack(token);
+}
+
+Pattern::PatternItem::PatternItem(TokenType token, const List<KeywordType>& keyword, const PatternItem& next) : keywords(keyword) {
+	tokens.PushBack(token);
+	if (next.next.GetSize()) this->next.PushBack(next);
+}
+
+Pattern::PatternItem::PatternItem(const List<TokenType>& tokens, const List<KeywordType>& keyword, const List<PatternItem>& next) : keywords(keyword), tokens(tokens), next(next) {
+}
+
+Pattern::PatternItem::PatternItem(const List<TokenType>& tokens, const List<KeywordType>& keyword, const PatternItem& next) : keywords(keyword), tokens(tokens) {
+	if (next.next.GetSize()) this->next.PushBack(next);
+}
+
+
+Pattern::PatternItem::~PatternItem() {
+	/*for (uint64 i = 0; i < next.GetSize(); i++) {
+		delete next[i];
+	}*/
+}
